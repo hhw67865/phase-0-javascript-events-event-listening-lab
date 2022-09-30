9165d97713f584032a89cda3101cf84d0665a8c2
Henry Wu <hhw67865@gmail.com>
2022-09-30 10:45:10 -0400

    commit tree 0ff613886311997f065482e634cb60590072f46f

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+function clickAlert() {
+  alert('YOUR COMPUTER HAS A VIRUS');
+}
+
+button.addEventListener('click', clickAlert);
 }
